83891d326a4c2dee580f8ce878e50a5f3331d75b
Slim Jim <youssefibrahim909@gmail.com>
2024-08-21 20:10:40 -0500

    added rest of methods and top to keep track

--- src/Stack.java
+++ src/Stack.java
@@ -5,6 +5,9 @@ public class Stack<E> {
     
     private final int DEFAULT = 20;
     private E[] array;
+    //Top is used to keep track of the top element of the array.
+    //credit: https://www.geeksforgeeks.org/how-to-implement-stack-in-java-using-array-and-generics/
+    private int top;
 
 
     //default constructor
@@ -12,18 +15,58 @@ public class Stack<E> {
     public Stack(Class<E> clazz){
 
         this.array = (E[]) Array.newInstance(clazz, DEFAULT);
-
+        this.top = -1;
     }
-
     //constructor with a specific size
     public Stack(Class<E> clazz, int capacity){
 
 
         this.array = (E[]) Array.newInstance(clazz, capacity);
-        
+        this.top = -1;
+  
         
     }
 
+    public void push(E i){
+
+        if(top == array.length -1 ){
+            //throw exception
+        }
+
+        top = top + 1 ;
+
+        array[top] = i;
+    
+    }
+
+    public E pop(){
+
+        if(top == -1){
+            //exceptoion stack is empty
+        }
+
+        E item = array[top];
+        array[top] = null;
+
+        top = top - 1 ;
+
+        return item;
+
+    }
+
+    public E peek(){
+
+        if(top == -1){
+            //exceptoion stack is empty
+        }
+
+  
+        return array[top];
+    }
+
+    public boolean empty(){
+        return top ==-1;
+    }
 
 
 }
